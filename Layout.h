52ce71a27ad6e0b575bac03106b38bc1d4212946
Anna Libahunt <annaj6gi@gmail.com>
2015-11-15 23:36:56 +0200

    Basis for RaspberryPi + Arduino maze robot

---
+++ Layout.h
@@ -0,0 +1,12 @@
+const byte motorRightFw = ;
+const byte motorRightBw = ;
+const byte motorLeftFw = ;
+const byte motorLeftBw = ;
+
+const byte wheelSensorRight = ;
+const byte wheelSensorLeft = ;
+
+const byte sensorFront = ;
+const byte sensorRight = ;
+const byte sensorLeft =  ;
+const byte sensorBack = ;
---
+++ robot15_arduino.ino
@@ -0,0 +1,24 @@
+/*******************************************************************************
+Code for a micromouse (https://en.wikipedia.org/wiki/Micromouse) competition robot.
+Works in cooperation with RasperryPi.
+
+by Anna Jõgi a.k.a Libahunt
+
+*******************************************************************************/
+
+
+#define DEBUG /*comment this line out in production then all Serial instructsions are ignored*/
+#include "DebugUtils.h"/*leave this in, otherwise you get errors*/
+
+
+#include "Layout.h" //Pin connections
+#include "Settings.h" //Variables that might need tweaking
+#include "Vars.h"
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
